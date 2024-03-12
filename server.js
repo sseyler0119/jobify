8c271b8ed3e9e61c75dd036576c1e99f747cfdd0
Serena Seyler <sseyler0119@gmail.com>
2024-03-12 15:39:43 -0700

    connect front-end and back-end through proxy

--- server.js
+++ server.js
@@ -7,29 +7,36 @@ const app = express();
 
 import morgan from 'morgan';
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
 // routers
 import jobRouter from'./routes/jobRouter.js';
+import authRouter from'./routes/authRouter.js';
+import userRouter from'./routes/userRouter.js';
 
 // middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
-
+import { authenticateUser } from './middleware/authMiddleware.js';
 
 // middleware
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('hello');
 });
-app.post('/', (req, res) => {
-  console.log(req);
-  res.json({ message: 'Data received', data: req.body });
-});
 
-app.use('/api/v1/jobs', jobRouter); 
+app.get('/api/v1/test', (req, res) => {
+  res.json({msg: "test route"})
+})
+
+
+app.use('/api/v1/jobs', authenticateUser, jobRouter); 
+app.use('/api/v1/auth', authRouter); 
+app.use('/api/v1/users',authenticateUser, userRouter); 
 
 // not found
 app.use('*', (req, res) => {
