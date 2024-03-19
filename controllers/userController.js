cc0fd428d564b9b17f09621a13ac4845951c298a
Serena Seyler <sseyler0119@gmail.com>
2024-03-19 14:17:21 -0700

    fix update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/userModel.js';
 import Job from '../models/jobModel.js';
 import cloudinary from 'cloudinary';
-import {promises as fs} from 'fs';
+import { formatImage } from '../middleware/multerMiddleware.js';
 
 export const getCurrentUser = async (req, res) => {
     const user = await User.findOne({_id: req.user.userId})
@@ -21,12 +21,11 @@ export const updateUser = async (req, res) => {
       delete newUser.password; // remove password if exists
 
     if(req.file) {
-        const response = await cloudinary.v2.uploader.upload(req.file.path);
-        await fs.unlink(req.file.path);
-        console.log(response);
+        const file = formatImage(req.file);
+        
+        const response = await cloudinary.v2.uploader.upload(file);
         newUser.avatar = response.secure_url;
         newUser.avatarPublicId = response.public_id;
-        console.log(newUser);
     }
 
     const updatedUser = await User.findByIdAndUpdate(req.user.userId, newUser);
